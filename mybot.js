ed1f851a8733e3a73632b3fabac9cb2d90e1dcb3
Dventura02 <douglaslv96@gmail.com>
2017-07-30 01:49:14 -0600

    commit
    -config(token y prefix info)
    -prefix
    -prefix command

--- mybot.js
+++ mybot.js
@@ -4,12 +4,13 @@
 
 // Import the discord.js module
 const Discord = require('discord.js');
-
+const config = require("./config.json");
+const fs = require("fs");
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'MzM5NDE1OTUzODg2NDEyODEw.DF7tUg.5NaGYLnwXnCBVWBCB-6QXPFFl_o';
+const token = config.token;
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -20,11 +21,24 @@ client.on('ready', () => {
 // Create an event listener for messages
 client.on('message', message => {
 	console.log(message.content);
+	if (!message.content.startsWith(config.prefix)|| message.author.bot)
+		return;
     // If the message is "ping"
-    if (message.content.startsWith("ping")) {
+    if (message.content.startsWith(config.prefix + "ping")) 
     // Send "pong" to the same channel
     message.channel.send('pong');
-}
+
+    if(message.content.startsWith(config.prefix + "prefix")) {
+ 		 // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
+ 	 let newPrefix = message.content.split(" ").slice(1, 2)[0];
+ 	 // change the configuration in memory
+ 	 config.prefix = newPrefix;
+
+  	// Now we have to save the file.
+  	fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
+  	message.channel.send("New prefix: "+config.prefix);
+	}
+
 });
 
 // Log our bot in
